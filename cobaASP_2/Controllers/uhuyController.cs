875819adf82def0addc743470d08628f2dc81905
Nugrahanto <rianirsyahnugrahanto@gmail.com>
2017-01-07 00:06:57 +0700

    Add project files.

---
+++ cobaASP_2/Controllers/uhuyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cobaASP_2.Controllers
+{
+    public class uhuyController : Controller
+    {
+        //
+        // GET: /uhuy/
+
+        public ActionResult uhuy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ cobaASP_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cobaASP_2.MvcApplication" Language="C#" %>
